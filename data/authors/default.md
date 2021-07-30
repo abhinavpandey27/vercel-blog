e8bb6f6e080bf914d7326f670a36c0ccbbba4693
Abhinav Pandey <adhinavpandey@Adhinavs-MacBook-Pro.local>
2021-07-30 21:01:40 +0530

    First Commit :)

---
+++ data/authors/default.md
@@ -0,0 +1,15 @@
+---
+name: Abhinav Pandey
+avatar: /static/images/avatar.png
+
+email: abhinavpandey027@gmail.com
+twitter: https://twitter.com/ab27hi
+linkedin: https://www.linkedin.com/in/abhinav-pandey-nit/
+
+---
+
+Hello there 👋🏼, I am Abhinav Pandey.
+
+I am a 3rd Year Engineering student at NIT, Surat. Startups, design, and products fascinate me and I am currently exploring them at MPL.
+
+When I am not applying auto-layout to every layer on Figma, I am busy reading obscure blogs from bloggers who were last active in 2014, listening to music or exploring new YT channels.
---
+++ data/projectsData.js
@@ -0,0 +1,34 @@
+const projectsData = [
+  {
+    title: 'Pocket Redesign',
+    description: `Took a stab at solving a problem I faced using a product, through design of course.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'The Time Machine',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten handphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'The Time Machine',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten handphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'The Time Machine',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten handphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
